b84055ea656a3d7381f46f747c6902188e9cb8d3
quarkusio <no-reply@quarkus.io>
2026-04-13 11:26:33 +0000

    Initial commit

---
+++ src/test/java/com/callash/gottadothat/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package com.callash.gottadothat;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
