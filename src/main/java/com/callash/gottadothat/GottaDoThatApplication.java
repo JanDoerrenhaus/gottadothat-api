2cd84335b4a405616899c6ec9813f80b53b6321a
Jan Doerrenhaus <j.doerrenhaus@disphere.com>
2026-04-13 15:00:52 +0200

    It actually starts now.

---
+++ src/main/java/com/callash/gottadothat/GottaDoThatApplication.java
@@ -0,0 +1,9 @@
+package com.callash.gottadothat;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("api")
+public class GottaDoThatApplication extends Application
+{
+}
